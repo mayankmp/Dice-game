e65c992baa0887ebf872f7d0f483d31698f76f4d
mayankmp <mayankmp@users.noreply.github.com>
2023-09-01 07:52:23 +0000

    deploy: e0bfa524881217b0883c43bac1700e0c316a6897

--- assets/index-2c79987b.js
+++ assets/index-2c79987b.js
@@ -73,7 +73,7 @@ Error generating stack: `+o.message+`
     border: 1px solid transparent;
     color: white;
   }
-`,cm=({toggle:e})=>F.jsxs(fm,{children:[F.jsx("div",{children:F.jsx("img",{src:"/images/dices.png"})}),F.jsxs("div",{className:"content",children:[F.jsx("h1",{children:"Dice Game"}),F.jsx(Qu,{onClick:e,children:"Play Now"})]})]}),fm=Pt.div`
+`,cm=({toggle:e})=>F.jsxs(fm,{children:[F.jsx("div",{children:F.jsx("img",{src:"./images/dices.png"})}),F.jsxs("div",{className:"content",children:[F.jsx("h1",{children:"Dice Game"}),F.jsx(Qu,{onClick:e,children:"Play Now"})]})]}),fm=Pt.div`
   max-width: 1180px;
   height: 100vh;
   display: flex;
